3560fde5b84eb35950e99f461bdb3a2d747f4749
saydar31 <ajdarshaydullin@gmail.com>
2025-03-13 17:37:45 +0300

    add client placeholders

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
+import {Stack} from 'react-bootstrap-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Bootstrap Navbar */}
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home"><Stack/>MasterBI</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+
+      {/* Bootstrap Grid with Placeholders */}
+      <Container className="my-4">
+        <Row>
+          <Col md={4}>
+            <div className="placeholder" style={{ height: '200px', backgroundColor: '#e9ecef' }}>Placeholder 1</div>
+          </Col>
+          <Col md={4}>
+            <div className="placeholder" style={{ height: '200px', backgroundColor: '#e9ecef' }}>Placeholder 2</div>
+          </Col>
+          <Col md={4}>
+            <div className="placeholder" style={{ height: '200px', backgroundColor: '#e9ecef' }}>Placeholder 3</div>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* Bootstrap Footer */}
+      <footer className="bg-light text-center text-lg-start mt-auto">
+        <div className="text-center p-3">
+          MasterBI
+        </div>
+      </footer>
     </div>
   );
 }
