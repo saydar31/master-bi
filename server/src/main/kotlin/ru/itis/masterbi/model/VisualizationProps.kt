3f2701f27fa3be8f89c018ac6ca228ceefd14454
saydar31 <ajdarshaydullin@gmail.com>
2025-04-28 16:00:37 +0300

    поправил VisualizationProps

--- server/src/main/kotlin/ru/itis/masterbi/model/VisualizationProps.kt
+++ server/src/main/kotlin/ru/itis/masterbi/model/VisualizationProps.kt
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.annotation.JsonValue
 
 class VisualizationProps {
     var color: String? = null
+    val backgroundColor: String? = null
     var shape: MarkerShape? = null
     var lineType: LineType? = null
+    var font: FontProps? = null
+
     var keyProps: MutableMap<String, VisualizationProps> = mutableMapOf()
 
     enum class MarkerShape {
@@ -16,7 +19,13 @@ class VisualizationProps {
     }
 
     enum class LineType {
-        DEFAULT, DOTTED
+        SOLID, DOTTED, DASHED
     }
+
+    data class FontProps(
+        var family: String? = null, // e.g., "Arial"
+        var size: Int? = null, // e.g., 14
+        var weight: String? = null // e.g., "bold"
+    )
 
 }
