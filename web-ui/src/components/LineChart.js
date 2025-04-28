e37b078cb5bb4a6a33bf0bda4ed158f3b51df177
saydar31 <ajdarshaydullin@gmail.com>
2025-04-28 16:01:04 +0300

    добавил проперти для лайнчарта

--- web-ui/src/components/LineChart.js
+++ web-ui/src/components/LineChart.js
@@ -22,6 +22,12 @@ ChartJS.register(
   Legend
 );
 
+const nameToDash = {
+  'SOLID': [],
+  'DOTTED': [5, 5],
+  'DASHED': [10, 5] 
+}
+
 const LineChart = ({ metadata, data }) => {
   const queryIds = metadata.queries.map(q => q.id)
   const labels = queryIds.flatMap(e => data[e].data)
@@ -30,12 +36,14 @@ const LineChart = ({ metadata, data }) => {
     labels: labels,
     datasets: metadata.queries.map(item => {
       const queryData = data[item.id].data || []
+      const borderDash = nameToDash[item.visualizationProps.lineType] || []
       return {
         label: item.label || 'Value',
         data: queryData.map(e => parseFloat(e.value)),
-        backgroundColor: item.visualizationProps.color || 'rgba(75, 192, 192, 0.5)',
+        backgroundColor: item.visualizationProps.backgroundColor || 'rgba(75, 192, 192, 0.5)',
         borderColor: item.visualizationProps.color || 'rgba(75, 192, 192, 1)',
         pointStyle: item.visualizationProps.shape || 'circle',
+        borderDash: borderDash,
         tension: 0.1
       }
     }),
